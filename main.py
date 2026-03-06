ecda43594a92722e9e69cbaef7234a12a4d1f4e5
r3ci <extiverooo@proton.me>
2026-03-06 22:33:09 +0100

    some of base

---
+++ main.py
@@ -0,0 +1,146 @@
+# r3ci is finally gonna respect the pep rules ???!?!?!?
+import curl_cffi
+from tqdm import tqdm
+from dataclasses import dataclass
+import os
+import uuid
+import shutil
+import threading
+import time
+import datetime
+
+@dataclass
+class Folders:
+    root: str = os.getcwd()
+    temp = os.path.join(root, 'temp')
+
+    installer: str = os.path.join(root, 'snapshots', 'installer')
+    installer_raw: str = os.path.join(root, 'snapshots', 'installer', 'raw')
+    installer_unpacked_layer1: str = os.path.join(root, 'snapshots', 'installer', 'unpacked_layer1')
+    installer_unpacked_layer2: str = os.path.join(root, 'snapshots', 'installer', 'unpacked_layer2')
+
+    app: str = os.path.join(root, 'snapshots', 'app')
+    app_raw: str = os.path.join(root, 'snapshots', 'app', 'raw')
+    app_unpacked: str = os.path.join(root, 'snapshots', 'app', 'unpacked')
+
+    uniextract: str = os.path.join(root, 'external', 'UniExtract', 'UniExtract.exe')
+
+for folder in [
+    Folders.root,
+    Folders.installer,
+    Folders.installer_raw,
+    Folders.installer_unpacked_layer1,
+    Folders.installer_unpacked_layer2,
+    Folders.app,
+    Folders.app_raw,
+    Folders.app_unpacked
+]:
+    os.makedirs(folder, exist_ok=True)
+
+class Utils:
+    def uniexcract(i, o):
+        os.system(f'{Folders.uniextract} {i} {o}')
+
+    def extract_version(file:str):
+        version = file.replace('Discord-', '').replace('-full.nupkg', '')
+        return version
+
+
+class DiscordTrolly:
+    def __init__(self):
+        self.sess = curl_cffi.requests.session.Session(impersonate='chrome142')
+        self.sess.headers.update({
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-encoding': 'gzip, deflate, br, zstd',
+            'accept-language': 'en-US;q=0.8,en;q=0.7',
+            'cache-control': 'no-cache',
+            'pragma': 'no-cache',
+            'priority': 'u=0, i',
+            'referer': 'https://www.google.com/',
+            'sec-ch-ua': '"Chromium";v="142", "Not-A.Brand";v="24", "Google Chrome";v="142"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
+        })
+    
+    def get_latest_installer(self, save_dir):
+        try:
+            url = 'https://discord.com/api/downloads/distributions/app/installers/latest'
+            params = {
+                'channel': 'stable',
+                'platform': 'win',
+                'arch': 'x64'
+            }
+
+            os.makedirs(save_dir, exist_ok=True)
+            filepath = os.path.join(save_dir, 'DiscordSetup.exe')
+
+            r = self.sess.get(url, params=params, stream=True, timeout=120)
+            r.raise_for_status()
+
+            total_size = int(r.headers.get('Content-Length', 0))
+            chunk_size = 1024 * 1024
+
+            with open(filepath, 'wb') as f, tqdm(
+                total=total_size, unit='B', unit_scale=True, desc='Downloading installer', miniters=1
+            ) as progress:
+                for chunk in r.iter_content(chunk_size=chunk_size):
+                    if chunk:
+                        f.write(chunk)
+                        progress.update(len(chunk))
+
+            print(f'Download complete: {filepath}')
+            return True
+        
+        except Exception as e:
+            print(f'Error downloading installer: {e}')
+            return False
+
+trolly = DiscordTrolly()
+
+def installer_loop():
+    while True:
+        datetime_rn = datetime.datetime.now().strftime('%Y-%m-%d')
+        raw_save_dir = os.path.join(Folders.installer_raw, datetime_rn)
+        succedded = trolly.get_latest_installer(raw_save_dir)
+        if succedded:
+            tempdir1 = os.path.join(Folders.temp, uuid.uuid4().hex)
+            os.makedirs(tempdir1, exist_ok=True)
+            Utils.uniexcract(os.path.join(raw_save_dir, 'DiscordSetup.exe'), tempdir1)
+            version = None
+            for file in os.listdir(tempdir1):
+                if file.startswith('Discord-'):
+                    version = Utils.extract_version(file)
+                    break
+            
+            if not version:
+                print('Failed to extract version')
+                continue
+
+            layer1_save_dir = os.path.join(Folders.installer_unpacked_layer1, version)
+            if not os.path.exists(layer1_save_dir):
+                os.makedirs(layer1_save_dir, exist_ok=True)
+
+                for file_name in os.listdir(tempdir1):
+                    src_path = os.path.join(tempdir1, file_name)
+                    dst_path = os.path.join(layer1_save_dir, file_name)
+                    
+                    if os.path.isfile(src_path):
+                        shutil.copy2(src_path, dst_path)
+                    elif os.path.isdir(src_path):
+                        shutil.copytree(src_path, dst_path, dirs_exist_ok=True)
+
+
+            os.remove(tempdir1)
+
+        time.sleep(600)
+
+threading.Thread(target=installer_loop, daemon=True).start()
+
+while True:
+    time.sleep(1)
